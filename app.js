3b874fffb3a1b3bd4d729db4647f1453865595a0
PrestonS95 <prestonsullins95@gmail.com>
2022-06-26 23:53:01 -0500

    updates

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // state
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const button = document.getElementById('start');
-button.addEventListener('click', buildBoard, buildInitialState)
+button.addEventListener('click', drawSnake, buildInitialState)
+
+
 let initialState;
+
+const boardSize = 20
+const columns = canvas.Height / boardSize
+const rows = canvas.Width / boardSize
+canvasSize = 20 * 20
+let time = 10
+let score = 0
+
 let snake = {
+    headxy: [] ,
     body: [ [10, 5], [10, 6], [10, 7], [10, 8] ],
     nextDirection: [1, 0]
   }
@@ -10,31 +23,41 @@ let snake = {
     apple: [11, 8],
     snake: snake // from above
   }
-let table = document.getElementsByTagName('table')[0]
-function makeRow(){
-    const row = document.createElement('tr')
-    for (let i = 0; i < 20; i++){
-        const td = document.createElement('td')
-        row.appendChild(td)
-    }
-    table.appendChild(row)
-}
-function buildBoard(){
-   for (let i = 0; i<20; i++){
-    makeRow()
-   }
-}
-
 
 function buildInitialState() {
+    canvas = document.getElementById('Canvas');
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'White'
+    ctx.fillRect = (boardSize, boardSize, canvasSize*boardSize-boardSize)
     
+    snake();
+    apple();
+   // setTimeout("gameCycle()", DELAY)
+    setInterval(tick, 1000 / 30)
+    drawSnake()
 }
 
 
 // render
 function renderState() {
+    
+       clearBoard()
+}
+
+function clearBoard(){
+   
+    ctx.fillStyle= 'black'
+    ctx.fillRect(0,0,canvas.clientHeight,canvas.clientWidth)
 
 }
+function drawSnake(){
+    ctx.fillStyle = 'green'
+    for(let i =0; i < snake.body.length; i++){
+        ctx.fillStyle = 'green';
+		ctx.fillRect(snake.body[i].x, snake.body[i].y, boardSize, boardSize);
+    }
+}
+drawSnake()
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
@@ -44,8 +67,8 @@ function onBoardClick() {
 
   renderState() // show the user the new state
 }
-const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
+;
+// board.addEventListener('click', onBoardClick); // etc
 
 
 // add to above
@@ -59,6 +82,27 @@ function tick() {
   setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
   
   // now you might have things like
-  document.addEventListener('keydown', function (onkeypress) {
-    // here you might read which key was pressed and update the state accordingly
-  })
+  document.addEventListener('keydown',function keyDown(e){
+  {
+
+    if(e.keyCode==38){
+        yvelocity=-1; //move one tile up
+        xvelocity=0;
+
+    }
+    if(e.keyCode==40){
+        yvelocity=1;//move one tile down
+        xvelocity=0;
+    }
+
+    if(e.keyCode==37){
+        yvelocity=0;
+        xvelocity=-1;       //move one tile left
+    }
+    if(e.keyCode==39){
+        yvelocity=0;
+        xvelocity=1;        //move one tile right
+    }
+}
+    
+})
